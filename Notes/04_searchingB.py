1d1db51197cde96f722af3e5585d8b110617d691
sammykagan <skagan@fwparker.org>
2019-02-26 13:32:44 -0600

    2/26/19 notes

--- Notes/04_searchingB.py
+++ Notes/04_searchingB.py
@@ -42,6 +42,52 @@ with open('data/villains.txt', 'r') as f:
 
 # To use the data, read it into a list
 with open('data/villains.txt') as Samé:
-    villain_list = [x.strip() for x in Samé]
+    villain_list = [x.strip().upper() for x in Samé]
 
 print(villain_list)
+
+# Linear Search
+key = "LEE THE MERCILESS" # what we are looking for
+i = 0 # the index of the loop
+
+while i < len(villain_list) and key != villain_list[i]:
+    i += 1 # loop will stop when it finds "THE DEADLY RAVEN"
+
+if i < len(villain_list):
+    print("Found", key, "at position", i)
+else:
+    print("Could not find", key)
+
+# Binary Search  --- LINDSAY he basically just played a game with us to demonstrate this point: https://www.geeksforgeeks.org/binary-search/
+villain_list.sort()
+print(villain_list)
+key = "RENARD THE TORTURER" #"Save big money at Renard's" -Nicky Lerner
+# Mr. Lee laughed
+# I laughed LOUDER
+
+
+lower_bound = 0
+upper_bound = len(villain_list) - 1
+found = False
+loops = 0 # counts the number of tries it too to find things
+# with 2^7 loops, you can search 2^7 items in just 7 tries
+
+
+# loop until we find it
+while lower_bound <= upper_bound and not found:
+    loops += 1
+    middle_pos = (upper_bound + lower_bound) // 2
+    if villain_list[middle_pos] < key:
+        lower_bound = middle_pos + 1
+    elif villain_list[middle_pos] > key:
+        upper_bound = middle_pos - 1
+    else:
+        found = True
+if found:
+    print("\nFound", key, "at position", middle_pos, "with", loops, "loops")
+else:
+    print("Could not find", key, "after", loops, "loops")
+
+# go here now: http://programarcadegames.com/index.php?chapter=lab_spell_check
+# this project is due at some random nebulous point in the future like all of our assignments
+
