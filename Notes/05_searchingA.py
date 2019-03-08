c601b2af5d19e037054ac87ccd65e0a07d424022
sammykagan <skagan@fwparker.org>
2019-03-08 09:04:14 -0600

    EVEN MORE SORTING! We're talking all kinds of sorting up in here. We watched Romanian Folk Dancers sort THEMSELVES so we could learn to sort OURSELVES.
    https://www.youtube.com/channel/UCIqiLefbVHsOAXDAxQJH7Xw

---
+++ Notes/05_searchingA.py
@@ -0,0 +1,55 @@
+# Friday, March 8
+# Day before Dubai
+
+# Sorting
+import random
+
+# Swap values
+a = 1
+b = 2
+print(a, b)
+
+temp = a # store it before we destroy
+a = b
+b = temp
+print(a, b)
+
+# the pythonic way
+a, b = b, a
+print(a, b)
+
+# make a random list of 100 numbers with value 1 to 99
+# use list comprehension
+
+# https://www.toptal.com/developers/sorting-algorithms (has algorithms, code, et. al.)
+
+# WATCH THIS VIDEO https://www.youtube.com/channel/UCIqiLefbVHsOAXDAxQJH7Xw
+
+randlist = [random.randrange(1, 100) for x in range(100)]
+print(randlist)
+
+def selection_sort(my_list):
+    for current_position in range(len(my_list)):
+        minimum_position = current_position
+        for scan_position in range(current_position + 1, len(my_list)):
+            if my_list[scan_position] < my_list[minimum_position]:
+                minimum_position = scan_position
+        my_list[current_position], my_list[minimum_position] = my_list[minimum_position], my_list[current_position]
+
+selection_sort(randlist)
+print(randlist)
+
+# Insertion Sort
+randlist = [random.randrange(1,100) for x in range(100)]
+print(randlist)
+
+for key_pos in range(1, len(randlist)):
+    key_val = randlist[key_pos]
+    scan_pos = key_pos - 1 # look to dancer on the left
+    while (scan_pos >= 0) and (randlist[scan_pos] > key_val):
+        randlist[scan_pos + 1] = randlist[scan_pos]
+        scan_pos -= 1
+
+    randlist[scan_pos + 1] = key_val
+
+print(randlist)
