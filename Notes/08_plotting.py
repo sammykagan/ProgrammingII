34a49f2a98d23800f205041a727c3afa2b0a6095
sammykagan <skagan@fwparker.org>
2019-03-25 10:37:54 -0500

    NOTES!

--- Notes/08_plotting.py
+++ Notes/08_plotting.py
@@ -28,4 +28,69 @@ plt.ylabel('intensity (dB)')
 plt.title('Example Plot')
 plt.axis([0, 50, 0, 1000]) # [xmin, xmax, ymin, ymax]
 
+#plt.show()
+
+import csv
+import matplotlib.pyplot as plt
+
+with open("data/Libraries_-_2018_Visitors_by_Location.csv") as f:
+    reader = csv.reader(f) # makes a reader object to pull in the data
+    data = list(reader) # cast the reader as a list
+
+print(data)
+
+header = data.pop(0)
+print(header)
+
+#LINDSAY THIS BIT RIHT HERE IS WHAT YOU NEED
+data.sort(key=lambda x: int(x[-1]))
+print(data)
+data.pop(-1)
+
+
+library_names = [x[0] for x in data]
+monthly_data = [x[1:-1] for x in data] # grabs data from the first value in the list to the end
+print(monthly_data)
+print(library_names)
+
+lp_data = monthly_data[library_names.index("Lincoln Park")]
+print(lp_data)
+
+try:
+    lp_data = [int(x) for x in lp_data]
+except:
+    print("Could not convert the data values to integers")
+
+print(lp_data)
+
+months = [x for x in header[1:-1]]
+
+plt.figure(3, tight_layout = True)
+plt.axis([0, 12, 0, 17000])
+plt.plot(lp_data)
+month_numbers = [x for x in range(12)]
+
+plt.xticks(month_numbers, months, rotation=45) # puts text on axis
+
+# Let's plots every library YTD attendance as a bar graph
+
+plt.figure(4, tight_layout = True) # makes sure the text doesn't run off the screen
+
+# x-axis list is library names
+library_numbers = [x for x in range(len(library_names))]
+print(library_numbers)
+
+# y-axis list is the "Year to Date" attendances
+try:
+    library_ytd = [int(x[-1]) for x in data]
+except:
+    print("Could not convert to integer")
+
+plt.bar(library_numbers, library_ytd, 1, alpha=1, color='white', edgecolor='hotpink', linewidth = 1)
+plt.xticks(library_numbers, library_names, rotation=90, fontsize=6)
+
+plt.xlabel("Chicago Library Branch")
+plt.ylabel("Total Visitors")
+plt.title("Chicago Public Library Visitors 2018")
+
 plt.show()
