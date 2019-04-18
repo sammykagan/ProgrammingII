075bd486774b398f829f21989f0b8308169a6c77
sammykagan <skagan@fwparker.org>
2019-04-18 14:31:14 -0500

    hey!

--- Notes/kivy/main.py
+++ Notes/kivy/main.py
@@ -1,11 +1,18 @@
 import kivy
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
 
-Window.size = (800, 300)
+Window.size = (600, 400)
 
 class TempApp(App):
+    def build(self):
+        return TempLayout()
+
+class TempLayout(BoxLayout):
+    #will contain all of my functions
     pass
+
 
 if __name__ == "__main__":
     app = TempApp()
